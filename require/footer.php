63d85f3159492178e8fd61558d5d885bf49a96c7
Tristan Ducrocq <tducrocq2612@gmail.com>
2022-05-09 15:10:10 +0200

    navbar + footer

---
+++ require/footer.php
@@ -0,0 +1,41 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start">
+    <section class="footer">
+        <!-- Informations  -->
+        <section class="footerbody">
+            <div class="container text-center text-md-start mt-5">
+                <div class="bod row mt-3">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase">Réservation sandwich Saint-Vincent</h6>
+                        <p>
+                            30 Rue de Meaux <br> 60300 SENLIS
+                        </p>
+                    </div>
+
+                    <!--Contact-->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase">Nous contacter</h6>
+                        <p class="tel">Tel: 03.44.53.96.40 </p>
+                        <p class="fax">Fax: 03.44.53.58.96</p>
+                        <p class="web">Web: https://www.lycee-stvincent.fr/</p>
+                    </div>
+
+                    <!-- Références -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase">Accès rapide</h6>
+                        <p>
+                            <a href="#!" class="refaccueil">Accueil</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="refcommandes">Vos commandes</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Copyright -->
+        <div class="copyright text-center p-3">
+            © 2022 Copyright: Armand DECORMEILLE / Tristan DUCROCQ / Loris DACA / Hugo PARAGEAUD
+        </div>
+    </section>
+</footer>
