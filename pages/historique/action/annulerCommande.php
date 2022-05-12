055f0f7b63de80deb4fd0b7f76b7e2675e06431a
Tristan Ducrocq <tducrocq2612@gmail.com>
2022-05-12 12:02:38 +0200

    page annulation commande

--- pages/historique/action/annulerCommande.php
+++ pages/historique/action/annulerCommande.php
@@ -16,9 +16,80 @@
         <!-- Header -->
         <?php require "../../../require/header.php" ?>
 
-        <section name='sectionFormulaireModifier'>
-            <form method="post"></form>
+        <section name='affichage'>
+            <table>
+                <tr>
+                    <th class='th textAlign'> Sandwich </th>
+                    <th class='th textAlign'> Boisson </th>
+                    <th class='th textAlign'> Dessert </th>
+                    <th class='th textAlign'> Chips </th>
+                    <th class='th textAlign'> Date commande </th>
+                    <th class='th textAlign'> Date livraison </th>
+                    <th class='th textAlign'> Commande annulée </th>   
+                </tr>
+
+                <?php
+                    $reqAfficher = $co->prepare("
+                    SELECT C.id_com, S.nom_sandwich, B.nom_boisson, D.nom_dessert, C.chips_com, C.date_heure_com, C.date_heure_livraison_com, C.annule_com
+                    FROM commande C, sandwich S, boisson B, dessert D
+                    WHERE C.id_com = :id_com
+                    AND C.fk_sandwich_id = S.id_sandwich
+                    AND C.fk_boisson_id = B.id_boisson
+                    AND C.fk_dessert_id = D.id_dessert");
+                    $reqAfficher->bindParam('id_com', $_GET['id']);
+                    $reqAfficher->execute();
+                    $afficher = $reqAfficher->fetchAll();
+
+                    foreach ($afficher as $resultat)
+                    {
+                        if($resultat['chips_com'] == 1)
+                        {
+                            $chips = "oui";
+                        }else{ $chips = "non";}
+                        if($resultat['annule_com'] == 1)
+                        {
+                            $annule = "oui";
+                        }else{ $annule = "non";}
+                        echo "<tr>";
+                            echo "<td class='tableau'>". $resultat['nom_sandwich'] ."</td>";
+                            echo "<td class='tableau'>". $resultat['nom_boisson'] ."</td>";
+                            echo "<td class='tableau'>". $resultat['nom_dessert'] ."</td>";
+                            echo "<td class='tableau'>". $chips ."</td>";
+                            echo "<td class='tableau'>". $resultat['date_heure_com'] ."</td>";
+                            echo "<td class='tableau'>". $resultat['date_heure_livraison_com'] ."</td>";
+                            echo "<td class='tableau'>". $annule ."</td>";
+                        echo "</tr>";
+                    }
+                ?>
+
+                <!--affichage des commandes utilisateur -->
+                <?php
+                ?>
+            </table>
         </section>
+        <section name='sectionFormulaireModifier'>
+            <form method="post">
+                <label for="reponse"> Voulez-vous annuler la commande ?</label>
+                <input type="submit" class="btnForm1" value="Oui" name="reponse">
+                <input type="submit" class="btnForm1" value="Non" name="reponse">
+            </form>
+        </section>
+        <?php
+            if(isset($_POST['reponse']))
+            {
+                $reponse = '0';
+                if($_POST['reponse'] == 'Oui')
+                {
+                    $reponse = '1';
+                    $id_commande = $_GET['id'];
+                    $reqAnnuler = $co->prepare("UPDATE commande SET annule_com = :reponse WHERE id_com = :id");
+                    $reqAnnuler->bindParam('id', $id_commande);
+                    $reqAnnuler->bindParam('reponse', $reponse);
+                    $reqAnnuler->execute();
+                }
+                header('Location: ../');
+            }
+        ?>
 
         <!-- Footer -->
         <?php require "../../../require/footer.php" ?>
