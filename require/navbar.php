27a0be72a1294020850a13a382b941871e4fc530
Armand45220 <decormeillej7@gmail.com>
2022-05-13 20:37:48 +0200

    ajout navbar

---
+++ require/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="navmarg container-fluid">
+        <a class="navbar-brand" href="#"><img src="css/image/logo.png" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Accueil</a>
+                </li>                          
+                <li class="nav-item2">
+                    <a class="nav-link" href=""> 
+                    <i class="fa-solid fa-right-to-bracket"></i> Se connecter</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
