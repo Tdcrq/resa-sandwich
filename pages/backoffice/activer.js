0e63281196fec7953788e46ebfacfb5ee47d4c74
tducrocq <tducrocq2612@gmail.com>
2022-05-15 18:24:28 +0200

    recup fichier hugo (bo)

---
+++ pages/backoffice/activer.js
@@ -0,0 +1,24 @@
+ function groupeForm(){ 
+    var $groupeForm =  document.getElementById('groupeForm'); 
+    $groupeForm.disabled = !$groupeForm.disabled; 
+}
+
+function groupeBouton(){ 
+    var $groupeBouton =  document.getElementById('groupeBouton'); 
+    $groupeBouton.disabled = !$groupeBouton.disabled; 
+}
+
+function groupeFormMdp(){ 
+    var $groupeFormMdp =  document.getElementById('groupeFormMdp'); 
+    $groupeFormMdp.disabled = !$groupeFormMdp.disabled; 
+}
+
+function groupeFormId(){ 
+    var $groupeFormId =  document.getElementById('groupeFormId');
+    $groupeFormId.disabled = !$groupeFormId.disabled;  
+}
+
+function message_delete() {
+    var $msg="Message sur la ligne 1.nMessage sur la ligne 2.n...";
+    alert($msg);
+}
---
+++ pages/backoffice/sup.php
@@ -0,0 +1,80 @@
+<?php
+    require("connexion.php");
+
+    $conn = connexionBD();
+
+    session_start();// recup des var de session
+    if(!isset($_SESSION['id_user']))//verification que l'utilisateur est bien connecté
+    {
+        header('Location: http://localhost/resa-sandwich-accueil/forms/form_conn.php');
+        exit();
+    }else{
+        $nameUser = $_SESSION['name_user'];//recuperation du nameUser
+        $idUser = $_SESSION['id_user'];//recuperation de l'idUser
+    }
+
+    if(isset($_POST["non_sup"]))
+    {
+        header('Location: index.php');
+    }
+
+    if(isset($_POST["oui_sup"]))
+    {
+        $id = $_GET['id'];
+        $query = $conn->prepare('SELECT COUNT(*) FROM commande WHERE fk_user_id = :id');
+        $query->bindParam(':id' , $id);
+        $query->execute();
+        $result = $query->fetch();
+
+        if($result[0] != 0)
+        {
+            header('Location: desa.php?id='.$_GET['id'].'');
+        }
+        else
+        {
+            $id = $_GET['id'];
+
+            $query = $conn->prepare('DELETE FROM utilisateur WHERE id_user = :id');
+            $query->bindParam(':id' , $id);
+            $query->execute();
+            header('Location: index.php');
+
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="activer.js"></script>
+    <title>sup</title>
+</head>
+<body>
+    <?php
+        require "form/navbar.php";
+    ?>
+
+    <div id="sup">
+        <h5>
+            Voulez-vous suprimer cet l'utilisateur ?
+        </h5>
+        <form method="post">
+            <button type="submit" id="oui" name="oui_sup">
+                oui
+            </button>
+            <button type="submit" id="non" name="non_sup">
+                non
+            </button>
+        </form>
+    </div>
+
+    <?php
+        require "form/footer.php";
+    ?>
+</body>
+</html>
